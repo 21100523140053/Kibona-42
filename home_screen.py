577008faec9bb39ae64a1fb84c4f77dac692af68
21100523140053 <ainuliwekibona195@gmail.com>
2024-09-02 12:29:50 -0700

    Add files via upload

--- home_screen.py
+++ home_screen.py
@@ -1,329 +1,200 @@
 from kivy.uix.screenmanager import Screen
+from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.lang import Builder
+from kivy.metrics import dp
 from kivy.properties import StringProperty, ObjectProperty, DictProperty
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.card import MDCard
-from kivymd.uix.navigationbar import MDNavigationBar, MDNavigationItem
+from kivymd.uix.hero import MDHeroFrom
+
 
 Builder.load_string('''
-<ImageLayout>
-    id: image_layout
-    orientation: 'vertical'
-    # size: (60, 80)
-    size_hint: (None, None)
-    height: 250
-    width: 200
-    radius: [4,4,4,4]
-    FitImage:
-        source: root.source
-        width: image_layout.width
-        height: image_layout.height
-        size: image_layout.size
-        radius: image_layout.radius
-        
-    
-<GuitarItem>
-    theme_bg_color: "Custom"
-    md_bg_color: "2d4a50"
-    id: card
-    size_hint_y:None
-    height:"200dp"
-    orientation:"horizontal"
-    radius:20
-    padding:"10dp"
-    on_press:app.change_screen('product_descriptions')
-    FitImage:
-        source: "images/guitar.png"
-        radius: card.radius
-    MDRelativeLayout:
-        BoxLayout:
-            orientation:"vertical" 
-            spacing: "3dp"
-            padding: "10dp"
-            MDLabel:
-                text: "Ibanez"
-                font_style: "Display"
-                role: "small"        
-            MDLabel:
-                text: "GRG121DX-BKF" 
-            MDLabel:
-                text: "$445,99"
-        
-<BaseMDNavigationItem>
+<HeroItem>
+    size_hint_y: None
+    height: "200dp"
+    radius: "10dp"
 
-    MDNavigationItemIcon:
-        icon: root.icon
+    MDSmartTile:
+        id: tile
+        size_hint: None, None
+        size: root.size
+        on_release: root.on_release()
 
-    MDNavigationItemLabel:
-        text: root.text
+        MDSmartTileImage:
+            id: image
+            source: "frontend/images/guitar.png"
+            radius: dp(10)
+
+        MDSmartTileOverlayContainer:
+            id: overlay
+            md_bg_color: 0, 0, 0, .5
+            adaptive_height: True
+            padding: "8dp"
+            spacing: "8dp"
+            radius: [0, 0, dp(10), dp(10)]
+
+            MDLabel:
+                text: root.tag
+                theme_text_color: "Custom"
+                text_color: "white"
+                adaptive_height: True
+
+
+
 
 <HomeScreen>:
     name: "home"
-    MDSliverAppbar:
-        background_color: "2d4a50"
-        hide_appbar: True
-        MDTopAppBar:
-            type: "small"
-            pos_hint: {"top": 1}
-            theme_bg_color: "Custom"
-            md_bg_color: '#F0866B'
-    
-            MDTopAppBarLeadingButtonContainer:
-    
-                MDActionTopAppBarButton:
-                    icon: "menu"
-    
-            MDTopAppBarTitle:
-                text: "Kariakoo"
-    
-            MDTopAppBarTrailingButtonContainer:
-    
-                MDActionTopAppBarButton:
-                    icon: "magnify"
-    
-                MDActionTopAppBarButton:
-                    icon: "account"
-    
-                MDActionTopAppBarButton:
-                    icon: "bell"
-        MDSliverAppbarHeader:
+    MDTopAppBar:
+        type: "medium"
+        pos_hint: {"top": 1}
+        theme_bg_color: "Custom"
+        md_bg_color: '#F0866B'
 
-            FitImage:
-                source: "images/guitar.png"
+        MDTopAppBarLeadingButtonContainer:
 
-        MDSliverAppbarContent:
-            id: content
-            orientation: "vertical"
-            padding: "12dp"
-            theme_bg_color: "Custom"
-            md_bg_color: "2d4a50"
-            
-    
-     
-    MDNavigationBar:
-        on_switch_tabs: app.on_switch_tabs(*args)
+            MDActionTopAppBarButton:
+                icon: "menu"
 
-        BaseMDNavigationItem
-            icon: "home"
-            text: "home"
-            active: True
+        MDTopAppBarTitle:
+            text: "Kariakoo"
 
-        BaseMDNavigationItem
-            icon: "bookmark"
-            text: "bookmark"
+        MDTopAppBarTrailingButtonContainer:
 
-        BaseMDNavigationItem
-            icon: "message"
-            text: "message"
-        BaseMDNavigationItem
-            icon: "cart"
-            text: "Cart"
-        BaseMDNavigationItem
-            icon: "store"
-            text: "kariakoo"
-            
+            MDActionTopAppBarButton:
+                icon: "magnify"
+
+            MDActionTopAppBarButton:
+                icon: "account"
+
+            MDActionTopAppBarButton:
+                icon: "dots-vertical"
+
+    MDScrollView:
+        do_scroll_y: True
+        effect_cls: 'ScrollEffect'
+        bar_width: 0
+
+        BoxLayout:
+            adaptive_height: True
+            orientation: 'vertical'
+            spacing: 20
+
+            Label:
+                text: 'Top Linking'
+                size_hint_y: None
+                height: self.texture_size[1]
+                bold: True
+                padding: '10dp', 0
+
+            ScrollView:
+                do_scroll_x: True
+                do_scroll_y: False
+                bar_width: 0
+
+                BoxLayout:
+                    id: top_linking
+                    orientation: 'horizontal'
+                    size_hint_y: None
+                    height: 60  # Adjust as needed for item height
+                    size_hint_x: None
+                    adaptive_width: True  # Expands based on content
+                    spacing: 10
+
+            Label:
+                text: 'Top Sales'
+                size_hint_y: None
+                height: self.texture_size[1]
+                bold: True
+                padding: '10dp', 0
 
 
-                
-                        
+
 
 <Descriptions>:
     name: "product_descriptions"
-    MDBoxLayout: 
-        orientation: "vertical"
-        MDTopAppBar:
-            type: "small"
-            pos_hint: {"top": 1}
-            theme_bg_color: "Custom"
-            md_bg_color: "2d4a50"
-    
-            MDTopAppBarLeadingButtonContainer:
-    
-                MDActionTopAppBarButton:
-                    icon: "arrow-left"
-                    on_press: app.change_screen('home')
-    
-            MDTopAppBarTitle:
-                text: "Product View"
-    
-            MDTopAppBarTrailingButtonContainer:
-    
-                MDActionTopAppBarButton:
-                    icon: "magnify"
-    
-                MDActionTopAppBarButton:
-                    icon: "account"
-    
-                MDActionTopAppBarButton:
-                    icon: "dots-vertical"
-                
-        MDScrollView:
-            do_scroll_y: True
-            # effect_cls: 'ScrollEffect'
-            bar_width: 0
-            MDBoxLayout:
-                adaptive_height: True
-                orientation: 'vertical'
-                spacing: 4
-                MDLabel:
-                    text: 'Product Image'
-                    size_hint_y: None
-                    height: self.texture_size[1]
-                    bold: True
-                    padding: '10dp', 0
-                MDBoxLayout:
-                    height: 250
-                    size_hint_y: None
-                    ScrollView:
-                        do_scroll_x: True
-                        do_scroll_y: False
-                        bar_width: 0
-                        MDBoxLayout: 
-                            id: image_layout  
-                            size_hint: None, .85
-                            orientation: 'horizontal'
-                            adaptive_width: True
-                            spacing: 5
-                            padding: [5, 0, 0, 0]
-                            ImageLayout:
-                                source: "images/guitar.png"
-                            ImageLayout:
-                                source: "images/guitar.png"
-                            ImageLayout:
-                                source: "images/guitar.png"
-                            ImageLayout:
-                                source: "images/guitar.png"        
-                MDBoxLayout:
-                    orientation: 'vertical'
-                    size_hint_y: None
-                    adaptive_height: True
-                    radius: [10, 10, 0, 0]
-                    padding: [0, 0, 0, 4]
-                    theme_bg_color: "Custom"
-                    md_bg_color: "2d4a50"
-                    MDLabel:
-                        text: 'Product Descriptions:'
-                        size_hint_y: None
-                        height: self.texture_size[1]
-                        bold: True
-                        padding: '10dp', 0
-                    MDListItem:
-                        theme_bg_color: "Custom"
-                        md_bg_color: "2d4a50"
-                        MDListItemLeadingIcon:
-                            icon: "note"
-                    
-                        MDListItemHeadlineText:
-                            text: "Product Name"
-                    
-                        MDListItemSupportingText:
-                            text: "Supporting text"
-                    
-                        MDListItemTrailingIcon:
-                            icon: "star"
-                            
-                    MDListItem:
-                        theme_bg_color: "Custom"
-                        md_bg_color: "2d4a50"
-                        MDListItemLeadingIcon:
-                            icon: "note"
-                    
-                        MDListItemHeadlineText:
-                            text: "Product Reviews"
-                    
-                        MDListItemSupportingText:
-                            text: "Supporting text"
-                    
-                        MDListItemTrailingIcon:
-                            icon: "star"
-                            
-                    MDListItem:
-                        theme_bg_color: "Custom"
-                        md_bg_color: "2d4a50"
-                        MDListItemLeadingIcon:
-                            icon: "note"
-                    
-                        MDListItemHeadlineText:
-                            text: "Product Price"
-                    
-                        MDListItemSupportingText:
-                            text: "Supporting text"
-                    
-                        MDListItemTrailingIcon:
-                            icon: "star"
-                            
-                    MDListItem:
-                        theme_bg_color: "Custom"
-                        md_bg_color: "2d4a50"
-                        MDListItemLeadingIcon:
-                            icon: "note"
-                    
-                        MDListItemHeadlineText:
-                            text: "Product Specifications"
-                    
-                        MDListItemSupportingText:
-                            text: "Supporting text"
-                    
-                        MDListItemTrailingIcon:
-                            icon: "star"
-                            
-                    MDListItem:
-                        theme_bg_color: "Custom"
-                        md_bg_color: "2d4a50"
-                        MDListItemLeadingIcon:
-                            icon: "note"
-                    
-                        MDListItemHeadlineText:
-                            text: "Product Description"
-                    
-                        MDListItemSupportingText:
-                            text: "Please ensure you have the appropriate permissions. have no cols or rows set, layout is not triggered,have no cols or rows set, layout is not triggered"
-                    
-                        MDListItemTrailingIcon:
-                            icon: "star"
-                    MDListItem:
-                        theme_bg_color: "Custom"
-                        md_bg_color: "2d4a50"
-                        MDListItemLeadingIcon:
-                            icon: "note"
-                    
-                        MDListItemHeadlineText:
-                            text: "Product Shipping Cost"
-                    
-                        MDListItemSupportingText:
-                            text: "Supporting text"
-                    
-                        MDListItemTrailingIcon:
-                            icon: "star"
-                    
-                        
-                    
-    MDBoxLayout:
-        
-'''
-                    )
+    heroes_to: [hero_to]
+    MDTopAppBar:
+        type: "medium"
+        pos_hint: {"top": 1}
+        theme_bg_color: "Custom"
+        md_bg_color: '#F0866B'
+
+        MDTopAppBarLeadingButtonContainer:
+
+            MDActionTopAppBarButton:
+                icon: "arrow-left"
+
+        MDTopAppBarTitle:
+            text: "Product View"
+
+        MDTopAppBarTrailingButtonContainer:
+
+            MDActionTopAppBarButton:
+                icon: "magnify"
+
+            MDActionTopAppBarButton:
+                icon: "account"
+
+            MDActionTopAppBarButton:
+                icon: "dots-vertical"
+
+    MDHeroTo:
+        id: hero_to
+        size_hint: 1, None
+        height: "220dp"
+        pos_hint: {"top": 1}
+
+''')
 
 
-class GuitarItem(MDCard):
-    pass
-
-
-class BaseMDNavigationItem(MDNavigationItem):
-    icon = StringProperty()
+class HeroItem(MDHeroFrom):
     text = StringProperty()
+    manager = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ids.image.ripple_duration_in_fast = 0.05
+
+        def on_transform_in(self, instance_hero_widget, duration):
+            for instance in [
+                instance_hero_widget,
+                instance_hero_widget._overlay_container,
+                instance_hero_widget._image,
+            ]:
+                Animation(radius=[0, 0, 0, 0], duration=duration).start
+                (instance)
+
+        def on_transform_out(self, instance_hero_widget, duration):
+            for instance, radius in {
+                instance_hero_widget: [dp(10), dp(10), dp(10), dp(10)],
+                instance_hero_widget._overlay_container:
+                    [0, 0, dp(10), dp(10)],
+                instance_hero_widget._image: [dp(10), dp(10), dp(10), dp(10)],
+            }.items():
+                Animation(
+                    radius=radius,
+                    duration=duration,
+                ).start(instance)
+
+    def on_release(self):
+        def switch_screen(*args):
+            self.manager.current_heroes = [self.tag]
+            self.manager.screens[7].ids.hero_to.tag = self.tag
+            self.manager.current = "product_description"
+
+        Clock.schedule_once(switch_screen, .1)
 
 
 class HomeScreen(Screen):
     def on_enter(self):
-        for x in range(10):
-            self.ids.content.add_widget(GuitarItem())
+        for i in range(12):
+            hero_item = HeroItem(
+                text=f"Item {i + 1}", tag=f"Tag {i}", manager=self.manager
+            )
+            if not i % 2:
+                hero_item.md_bg_color = "lightgrey"
+            self.ids.top_linking.add_widget(hero_item)
 
 
 class Descriptions(Screen):
     source = StringProperty()
     content = DictProperty(dict())
-
-
-class ImageLayout(MDBoxLayout):
-    source = StringProperty()
